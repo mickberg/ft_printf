7597e1ab211c6136859afb2661710838910afb0e
mickberg <mikael.berglund2@gmail.com>
2020-02-05 18:52:10 +0200

    cleans includes

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mikaelberglund <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 08:10:25 by mikaelber         #+#    #+#             */
-/*   Updated: 2020/02/04 16:43:56 by mikaelber        ###   ########.fr       */
+/*   Updated: 2020/02/05 18:50:41 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,26 @@
 # include "ft_printf_types.h"
 # include "../libft/libft.h"
 # include <stdarg.h>
-# include <stdlib.h>
 # include <unistd.h>
 
-/*
-** Core
-*/
-int			ft_printf(const char *format, ...);
-int			ft_vprintf(const char *format, va_list ap);
+int		ft_printf(const char *format, ...);
+int		ft_vprintf(const char *format, va_list ap);
 
-/*
-** Parsing
-*/
-void		parse_format(t_format *info, const char *format, int *pos, va_list ap);
-void		parse_flags(t_format *info, const char *format, int *pos);
-void		parse_width(t_format *info, const char *format, int *pos, va_list ap);
-void		parse_precision(t_format *info, const char *format, int *pos, va_list ap);
-void		parse_length(t_format *info, const char *format, int *pos);
-void		parse_specifier(t_format *info, const char *format, int *pos);
-void		parse_number(const char *format, int *pos, int *num);
-void		format_bin(t_format *info, t_output *out, va_list ap);
+void	parse_format(t_format *info, const char *format, int *pos, va_list ap);
+void	parse_flags(t_format *info, const char *format, int *pos);
+void	parse_width(t_format *info, const char *format, int *pos, va_list ap);
+void	parse_precision(t_format *info, const char *format, int *pos, va_list ap);
+void	parse_length(t_format *info, const char *format, int *pos);
+void	parse_specifier(t_format *info, const char *format, int *pos);
+void	parse_number(const char *format, int *pos, int *num);
 
-/*
-** Arguments
-*/
 t_64	number_argument_signed(t_len lflag, va_list ap);
 t_u64	number_argument_unsigned(t_len lflag, va_list ap);
 t_f128	float_argument_signed(t_len lflag, va_list ap);
 
-/*
-** General Formatting
-*/
 void	(*format_router(t_format *info))(t_format*, t_output*, va_list);
 void	format_width(t_format *info, t_output *out, char *argstr, char *prefix);
 
-/*
-** Specifier Formatting
-*/
 void	format_int(t_format *info, t_output *out, va_list ap);
 void	format_uint(t_format *info, t_output *out, va_list ap);
 void	format_hex(t_format *info, t_output *out, va_list ap);
@@ -62,10 +45,8 @@ void	format_float(t_format *info, t_output *out, va_list ap);
 void	format_char(t_format *info, t_output *out, va_list ap);
 void	format_str(t_format *info, t_output *out, va_list ap);
 void	format_perc(t_format *info, t_output *out, va_list ap);
+void	format_bin(t_format *info, t_output *out, va_list ap);
 
-/*
-** UTILS
-*/
 void	ft_itos(long long num, char *dest);
 t_u64	ft_iabs(t_64 num);
 t_f128	ft_fabs(t_f128 num);
--- includes/ft_printf_types.h
+++ includes/ft_printf_types.h
@@ -6,7 +6,7 @@
 /*   By: mikaelberglund <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 07:54:13 by mikaelber         #+#    #+#             */
-/*   Updated: 2020/02/04 16:27:34 by mikaelber        ###   ########.fr       */
+/*   Updated: 2020/02/05 18:50:00 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,20 @@
 
 #include <stdlib.h>
 
-/*
-** structs and enum typedefs
-*/
 typedef struct s_format		t_format;
 typedef struct s_output		t_output;
 typedef enum e_length		t_len;
 typedef enum e_spec			t_spec;
-
-/*
-** Native types short hands
-*/
 typedef long long			t_64;
 typedef unsigned long long	t_u64;
 typedef long double			t_f128;
 
-/*
-** Flags, defined in bits because not all flags are exlusive
-*/
 # define FLAG_MINUS			0x01
 # define FLAG_PLUS			0x02
 # define FLAG_POUND			0x04
 # define FLAG_ZERO			0x08
 # define FLAG_SPACE			0x10
 
-/*
-** length
-*/
 enum						e_length
 {
 	len_none,
@@ -52,9 +39,6 @@ enum						e_length
 	len_longdouble,
 };
 
-/*
-** specifiers
-*/
 enum						e_spec
 {
 	spec_none,
@@ -71,9 +55,6 @@ enum						e_spec
 	spec_bin
 };
 
-/*
-** Format structure, holds format structure
-*/
 struct						s_format
 {
 	char		flags;
@@ -84,14 +65,10 @@ struct						s_format
 	t_spec		specifier;
 };
 
-/*
-** Output struct
-*/
 struct						s_output
 {
 	char		*string;
 	size_t		len;
-	size_t		arglen;
 };
 
 #endif
